6cf7992c4bc34b41b779d90229fdd08960937df9
Samma <46258134+Samunter@users.noreply.github.com>
2019-09-25 16:52:55 -0700

    create function to hide invalid color options

--- js/script.js
+++ js/script.js
@@ -18,6 +18,36 @@ $('#title').on('change', function(e) {
 const $noThemeColor = $(
   '<option value="select-theme">Please select a T-shirt theme</option>'
 );
-$('#color').append($noThemeColor);
 $('#color option').hide();
+$('#color').prepend($noThemeColor);
 $noThemeColor.attr('selected', true);
+
+//only display valid color options for selected theme
+$('#design').change(function(e) {
+  const $cornflowerBlue = $('option[value="cornflowerblue"]');
+  const $darkSlateGrey = $('option[value="darkslategrey"]');
+  const $gold = $('option[value="gold"]');
+  const $tomato = $('option[value="tomato"]');
+  const $steelBlue = $('option[value="steelblue"]');
+  const $dimGrey = $('option[value="dimgrey"]');
+  $('#design option')
+    .eq(0)
+    .hide();
+  if (e.target.value === 'js puns') {
+    $tomato.hide().attr('selected', false);
+    $steelBlue.hide();
+    $dimGrey.hide();
+    $noThemeColor.hide().attr('selected', false);
+    $cornflowerBlue.show().attr('selected', true); // Default value
+    $darkSlateGrey.show();
+    $gold.show();
+  } else if (e.target.value === 'heart js') {
+    $cornflowerBlue.hide().attr('selected', false);
+    $darkSlateGrey.hide();
+    $gold.hide();
+    $noThemeColor.hide().attr('selected, false');
+    $tomato.show().attr('selected', true); // Default value
+    $steelBlue.show();
+    $dimGrey.show();
+  }
+});
